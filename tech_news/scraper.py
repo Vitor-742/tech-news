4e007c6351fa06fdb6625e4444751933af06d83d
Vitor Campos <vitorcampos742@gmail.com>
2022-10-05 15:49:51 -0300

    requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -19,23 +19,31 @@ def fetch(url):
     return response.text
 
 
-# Requisito 2
 def scrape_novidades(html_content):
     selector = Selector(text=html_content)
     links = selector.css(".entry-title a::attr(href)").getall()
     return links
 
 
-# Requisito 3
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
     link = selector.css(".next::attr(href)").get()
     return link
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    summary = ''.join(selector.css(".entry-content > p:nth-of-type(1) *::text").getall())
+    return {
+        "url": selector.css('link[rel=canonical]::attr(href)').get(),
+        "title": selector.css('.entry-title::text').get().strip(),
+        "timestamp": selector.css('.meta-date::text').get(),
+        "writer": selector.css('.author a::text').get(),
+        "comments_count": 0,
+        "summary": summary.strip(),
+        "tags": selector.css('.post-tags a::text').getall(),
+        "category": selector.css('.label::text').get(),
+    }
 
 
 # Requisito 5
