4ec91093e9142f52959bcd94e38177ef6201ebad
Vitor Campos <vitorcampos742@gmail.com>
2022-10-06 18:35:27 -0300

    requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -6,6 +7,7 @@ def search_by_title(title):
     find_news = search_news({'title': {"$regex": title, '$options': 'i'}})
     # find_news = search_news({'title': f'/^{title}$/i'})
     list_news = []
+    print(find_news)
     for news in find_news:
         list_news.append((news['title'], news['url']))
     return list_news
@@ -13,7 +15,18 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+
+    try:
+        date_time = datetime.strptime(date, '%Y-%m-%d')
+        data = date_time.strftime("%d/%m/%Y")
+        find_news = search_news({'timestamp': data})
+        print(find_news)
+        list_news = []
+        for news in find_news:
+            list_news.append((news['title'], news['url']))
+        return list_news
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
